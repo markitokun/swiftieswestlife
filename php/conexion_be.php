92175c2e6151ae901e1299dce8d6ba670dd1229f
markitokun <119384614+markitokun@users.noreply.github.com>
2023-08-15 16:55:27 -0600

    Add files via upload

---
+++ php/conexion_be.php
@@ -0,0 +1,10 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_registrer_db");
+
+   /*if($conexion){
+        echo 'conecto';
+    }else{
+        echo 'no conecto';
+    }*/
+?>
