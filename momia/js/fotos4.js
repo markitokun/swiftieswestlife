483ec6ac1df9e50abeea99df29d1bdd4f0b692a0
markitokun <119384614+markitokun@users.noreply.github.com>
2023-11-25 16:16:09 -0600

    CHANGES

---
+++ momia/js/fotos4.js
@@ -0,0 +1,19 @@
+var outsideImages4 = ["img/parrk.jpg", "img/besopez.jpg", "img/lentes.jpg"];
+var outsideDescriptions4 = ["fotitos en el museo","besito de pez","nos cambiamos de lentitos"];
+var outsideCurrentIndex4 = 0;
+
+function changeOutsideContent4(offset) {
+    outsideCurrentIndex4 += offset;
+
+    if (outsideCurrentIndex4 < 0) {
+        outsideCurrentIndex4 = outsideImages4.length - 1;
+    } else if (outsideCurrentIndex4 >= outsideImages4.length) {
+        outsideCurrentIndex4 = 0;
+    }
+
+    updateOutsideContent4();
+}
+function updateOutsideContent4() {
+    document.getElementById("outsideImage4").src = outsideImages4[outsideCurrentIndex4];
+    document.getElementById("outsideDescription4").textContent = outsideDescriptions4[outsideCurrentIndex4];
+}
---
+++ momia/js/fotos5.js
@@ -0,0 +1,19 @@
+var outsideImages5 = ["img/xv1.jpg", "img/xv2.jpg", "img/xv3.jpg"];
+var outsideDescriptions5 = ["bien guapos asi formales","Te veias hermosa con ese vestido","LOCURA"];
+var outsideCurrentIndex5 = 0;
+
+function changeOutsideContent5(offset) {
+    outsideCurrentIndex5 += offset;
+
+    if (outsideCurrentIndex5 < 0) {
+        outsideCurrentIndex5 = outsideImages5.length - 1;
+    } else if (outsideCurrentIndex5 >= outsideImages5.length) {
+        outsideCurrentIndex5 = 0;
+    }
+
+    updateOutsideContent5();
+}
+function updateOutsideContent5() {
+    document.getElementById("outsideImage5").src = outsideImages5[outsideCurrentIndex5];
+    document.getElementById("outsideDescription5").textContent = outsideDescriptions5[outsideCurrentIndex5];
+}
